b8078acc309bd5a72cd3fbf69811cc9d447e07c6
vidaurre <dvidaurre@gmail.com>
2020-04-02 15:27:18 +0200

    add missing files

--- cvfolds.m
+++ cvfolds.m
@@ -1,1 +1,83 @@
-../shared/cvfolds.m
+function folds = cvfolds(Y,family,CVscheme,allcs)
+
+if nargin<4, allcs = []; end
+
+[N,q] = size(Y);
+if ~strcmp(family,'multinomial'), 
+    q = length(unique(Y)); 
+    if q<=3,
+       Y = nets_class_vectomat(Y);  
+    end
+elseif strcmp(family,'multinomial') && q==1,
+    Y = nets_class_vectomat(Y); q = size(Y,2);
+end
+
+if CVscheme==0, nfolds = N;
+else nfolds = CVscheme;
+end
+folds = {}; ifold = 1;
+grotDONE = zeros(N,1);
+if strcmp(family,'multinomial') || q<=3   % stratified CV that respects the family structure
+    counts = zeros(nfolds,q); Scounts = mean(Y); 
+    for k = 1:nfolds
+        if sum(grotDONE)==N, break; end
+        folds{ifold} = [];
+        while length(folds{ifold}) < ceil(N/nfolds)
+            d = Inf(N,1); j=1;
+            % find the family that best preserves the class proportions
+            grotDONEI = grotDONE;
+            while j<=N
+                if (grotDONEI(j)==0)
+                    Jj=[folds{ifold} j];
+                    if (~isempty(allcs))  % leave out all samples related to the one in question
+                        if size(find(allcs(:,1)==j),1)>0, Jj=[Jj allcs(allcs(:,1)==j,2)']; end
+                    end
+                    if length(Jj)>1, countsI = sum(Y(Jj,:)); % before: counts(LOfracI,:) + sum(Y(Jj,:));
+                    else countsI = Y(Jj,:); % before: counts(LOfracI,:) + Y(Jj,:);
+                    end
+                    countsI = countsI / sum(countsI);
+                    d(j) = sum( ( Scounts - countsI ).^2 ); % distance from the overall class proportions
+                    grotDONEI(Jj) = 1;
+                end
+                j=j+1;
+            end
+            % and assign it to this fold
+            [~,j] = min(d); j = j(1); 
+            folds{ifold}=[folds{ifold} j];
+            if (~isempty(allcs))  % leave out all samples related (according to cs) to the one in question
+                if size(find(allcs(:,1)==j),1)>0, folds{ifold}=[folds{ifold} allcs(allcs(:,1)==j,2)']; end
+            end
+            grotDONE(folds{ifold})=1; counts(k,:) = sum(Y(folds{ifold},:));
+            if k>1 && k<nfolds,
+                if sum(grotDONE)>k*N/nfolds, break; end
+            end
+        end
+        if ~isempty(folds{ifold}), ifold = ifold + 1; end
+    end
+else % standard CV respecting the family structure
+    for k = 1:nfolds
+        if sum(grotDONE)==N, break; end
+        j=1;  folds{ifold} = [];
+        while length(folds{ifold}) < ceil(N/nfolds) && j<=N
+            if (grotDONE(j)==0)
+                folds{ifold}=[folds{ifold} j];
+                if (~isempty(allcs))  % leave out all samples related to the one in question
+                    if size(find(allcs(:,1)==j),1)>0
+                        folds{ifold}=[folds{ifold} allcs(allcs(:,1)==j,2)'];
+                    end
+                end
+                grotDONE(folds{ifold})=1;
+            end
+            j=j+1;
+            if k>1 && k<nfolds,
+                if sum(grotDONE)>k*N/nfolds
+                    break
+                end
+            end
+        end
+        if ~isempty(folds{ifold}), ifold = ifold + 1; end
+    end
+end
+
+
+end
--- reshape_pred.m
+++ reshape_pred.m
@@ -1,1 +1,61 @@
-../shared/reshape_pred.m
+function Xin = reshape_pred(X,matrix_format,keepvar)
+% Reshape predictors to have covariance format (matrix_format==1) 
+% or vector format (matrix_format==0). 
+
+%
+% Usage:if matrix_format==1 and X is given vectorized,  
+%       keepvar==1 means that X contains the variance; 
+%       if matrix_format==0 and X is given in matrices format, 
+%       keepvar dictates if we keep the diagonal or not
+%       if matrix_format==0 and X is given vectorized,  
+%       keepvar==0 means that X contains the variance and we want to remove
+%       it; in this case, the variances are assumed to be ==1
+%       
+%       
+% Diego Vidaurre, University of Oxford (2015)
+
+
+N = size(X,1);
+if matrix_format==0 && length(size(X))==3 % just vectorize the matrices
+    Nnodes = size(X,2); 
+    if keepvar
+        Xin = zeros(N, Nnodes * (Nnodes+1) / 2);
+    else
+        Xin = zeros(N, Nnodes * (Nnodes-1) / 2);
+    end
+    for j=1:N
+        grot = permute(X(j,:,:),[2 3 1]);
+        Xin(j,:) = grot(triu(ones(Nnodes),~keepvar)==1);
+    end
+elseif matrix_format==1 && length(size(X))==2 % put in matrix format, and do riemann transform
+    if keepvar==0
+        Nnodes = (1 + sqrt(1+8*size(X,2))) / 2;
+    else
+        Nnodes = (-1 + sqrt(1+8*size(X,2))) / 2;
+    end
+    Xin = zeros(N, Nnodes, Nnodes);
+    for j=1:N
+        Xin(j,triu(ones(Nnodes),~keepvar)==1) = X(j,:);
+        grot = permute(Xin(j,:,:),[2 3 1]);
+        if keepvar
+            Xin(j,:,:) = grot + grot' - diag(diag(grot));
+        else
+            Xin(j,:,:) = grot + grot' + eye(Nnodes);
+        end
+    end
+elseif matrix_format==0 && length(size(X))==2 && keepvar==0 % remove the diagonal
+    Nnodes = (-1 + sqrt(1+8*size(X,2))) / 2;
+    Xin = zeros(N, Nnodes, Nnodes);
+    for j=1:N
+        ind1 = triu(ones(Nnodes),1)==1;
+        ind2 = all(abs(X-1)>eps);
+        Xin(j,ind1) = X(j,ind2);
+        grot = permute(Xin(j,:,:),[2 3 1]);
+        Xin(j,:,:) = grot + grot' + eye(Nnodes);
+    end
+    Xin = reshape_pred(Xin,0,0);
+else
+    Xin = X;
+end
+
+end
